455b8294d904441e74c2f37656653b58844f6e87
A.J. May <aj7may@gmail.com>
2019-06-11 12:39:20 -0400

    fix: add ports to exposedPorts in proxy config

--- src/proxyConfig.js
+++ src/proxyConfig.js
@@ -5,15 +5,32 @@ export default {
   Image: 'codekitchen/dinghy-http-proxy:latest',
   name: 'dotdocker-proxy',
   Env: ['CONTAINER_NAME=dotdocker-proxy'],
+  ExposedPorts: {
+    '80/tcp': {},
+    '443/tcp': {},
+    '19322/udp': {},
+  },
   HostConfig: {
     Binds: [
       '/var/run/docker.sock:/tmp/docker.sock:ro',
       `${resolve(homedir(), '.dotdocker/certs')}:/etc/nginx/certs:ro`,
     ],
     PortBindings: {
-      '80/tcp': [{ HostPort: '80' }],
-      '443/tcp': [{ HostPort: '443' }],
-      '19322/udp': [{ HostPort: '19322' }],
+      '19322/udp': [
+        {
+          HostPort: '19322',
+        },
+      ],
+      '443/tcp': [
+        {
+          HostPort: '443',
+        },
+      ],
+      '80/tcp': [
+        {
+          HostPort: '80',
+        },
+      ],
     },
     RestartPolicy: { Name: 'always' },
   },
