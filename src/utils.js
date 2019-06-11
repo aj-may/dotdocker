564fd5546618fd5e367984b0919c329f07d8794c
A.J. May <aj7may@gmail.com>
2019-06-11 13:45:26 -0400

    fix: remove hidden sudo prompt, throw error with instructions instead

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
 import Docker from 'dockerode';
-import { access } from 'fs';
 
 const docker = Docker();
 
@@ -12,5 +11,3 @@ export const getContainer = async name => {
   const containers = await docker.listContainers({ all: true, filters: { name: [name] } });
   return containers[0];
 };
-
-export const fileExists = path => new Promise(resolve => access(path, null, err => resolve(!err)));
