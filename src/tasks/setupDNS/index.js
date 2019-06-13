c64a7d20a0e798661777eb96b6934aed698bea61
A.J. May <aj7may@gmail.com>
2019-06-13 18:15:58 -0400

    feat: basic linux support

--- src/tasks/setupDNS/index.js
+++ src/tasks/setupDNS/index.js
@@ -1,10 +1,14 @@
 import { capitalize } from 'lodash';
 import darwinDnsSetup from './darwin';
+import linuxDnsSetup from './linux';
 
 const setupDNS = () => {
   switch (process.platform) {
     case 'darwin':
       return darwinDnsSetup;
+
+    case 'linux':
+      return linuxDnsSetup;
 
     default:
       return {
