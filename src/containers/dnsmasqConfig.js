2847878401dc01c79b66c09c0022f92c711d01d1
jtlacci <johntsterlacci@gmail.com>
2019-10-22 15:42:17 -0400

    feat: changed port 52 -> 822

--- src/containers/dnsmasqConfig.js
+++ src/containers/dnsmasqConfig.js
@@ -1,21 +1,21 @@
 export default {
   Image: 'andyshinn/dnsmasq:latest',
   name: 'dotdocker-dnsmasq',
-  Cmd: ['--address=/docker/127.0.0.1', '--log-facility=-'],
+  Cmd: ['-p 822:822/tcp', '-p 822:822/udp', '--address=/docker/127.0.0.1', '--log-facility=-'],
   ExposedPorts: {
-    '53/tcp': {},
-    '53/udp': {},
+    '822/tcp': {},
+    '822/udp': {},
   },
   HostConfig: {
     PortBindings: {
-      '53/tcp': [
+      '822/tcp': [
         {
-          HostPort: '53',
+          HostPort: '822',
         },
       ],
-      '53/udp': [
+      '822/udp': [
         {
-          HostPort: '53',
+          HostPort: '822',
         },
       ],
     },
