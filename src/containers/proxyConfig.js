ad5c04d9390af66d34fb85fa830ffecef6e9c7df
Yaniv Sofer <yaniv.sofer@ey.com>
2020-10-06 15:02:48 -0400

    feat: add ability to specify nginx_proxy.conf file

--- src/containers/proxyConfig.js
+++ src/containers/proxyConfig.js
@@ -1,5 +1,6 @@
 import { homedir } from 'os';
 import { resolve } from 'path';
+import fs from 'fs';
 
 export default {
   Image: 'codekitchen/dinghy-http-proxy:latest',
@@ -14,6 +15,14 @@ export default {
     Binds: [
       '/var/run/docker.sock:/tmp/docker.sock:ro',
       `${resolve(homedir(), '.dotdocker/certs')}:/etc/nginx/certs:ro`,
+      ...(fs.existsSync(`${resolve(homedir(), '.dotdocker/nginx_proxy.conf')}`)
+        ? [
+            `${resolve(
+              homedir(),
+              '.dotdocker/nginx_proxy.conf',
+            )}:/etc/nginx/conf.d/nginx_proxy.conf:ro`,
+          ]
+        : []),
     ],
     PortBindings: {
       '19322/udp': [
