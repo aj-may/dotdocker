5746483184397030a52594203bde61217aa9a0b6
mahboobmonnamd <mahboob@192.168.0.101>
2022-11-09 19:43:12 +0530

    fix: updated image name

--- src/containers/dnsmasqConfig.js
+++ src/containers/dnsmasqConfig.js
@@ -1,7 +1,7 @@
 const port = process.platform === 'darwin' ? '533' : '53';
 
 export default {
-  Image: 'andyshinn/dnsmasq:latest',
+  Image: '4km3/dnsmasq:latest',
   name: 'dotdocker-dnsmasq',
   Cmd: ['--address=/docker/127.0.0.1', '--log-facility=-'],
   ExposedPorts: {
